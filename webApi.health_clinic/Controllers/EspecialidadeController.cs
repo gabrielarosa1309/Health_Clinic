d587e91585a008604f38aeffe61e614b49acb6f8
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-09-29 10:47:49 -0300

    Repository e Controller de Especialidade concluídos

--- webApi.health_clinic/Controllers/EspecialidadeController.cs
+++ webApi.health_clinic/Controllers/EspecialidadeController.cs
@@ -1,6 +1,91 @@
-﻿namespace webApi.health_clinic.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using webApi.health_clinic.Domains;
+using webApi.health_clinic.Interfaces;
+using webApi.health_clinic.Repositories;
+
+namespace webApi.health_clinic.Controllers
 {
-    public class EspecialidadeController
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class EspecialidadeController : ControllerBase
     {
+        private EspecialidadeRepository _EspecialidadeRepository;
+
+        public EspecialidadeController()
+        {
+            _EspecialidadeRepository = new EspecialidadeRepository();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Guid id, Especialidade especialidadeUpdt)
+        {
+            try
+            {
+                _EspecialidadeRepository.Atualizar(id, especialidadeUpdt);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(Guid id)
+        {
+            try
+            {
+                return Ok(_EspecialidadeRepository.BuscarPorId(id));
+            }
+            catch (Exception e)
+            { 
+                return BadRequest(e.Message); 
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Especialidade especialidadeCrt)
+        {
+            try
+            {
+                _EspecialidadeRepository.Cadastrar(especialidadeCrt);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete ("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _EspecialidadeRepository.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet] 
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                return Ok(_EspecialidadeRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
--- webApi.health_clinic/Repositories/EspecialidadeRepository.cs
+++ webApi.health_clinic/Repositories/EspecialidadeRepository.cs
@@ -1,6 +1,63 @@
-﻿namespace webApi.health_clinic.Repositories
+﻿using webApi.health_clinic.Contexts;
+using webApi.health_clinic.Domains;
+using webApi.health_clinic.Interfaces;
+
+namespace webApi.health_clinic.Repositories
 {
-    public class EspecialidadeRepository
+    public class EspecialidadeRepository : IEspecialidadeRepository
     {
+        private readonly HealthClinicContext _healthClinicContext;
+
+        public EspecialidadeRepository()
+        {
+            _healthClinicContext = new HealthClinicContext();
+        }
+
+        public void Atualizar(Guid id, Especialidade especialidadeUpdt)
+        {
+            Especialidade especialidadeBuscada = _healthClinicContext.Especialidade.Find(id)!;
+
+            if (especialidadeBuscada != null)
+            {
+                especialidadeBuscada.TituloEspecialidade = especialidadeUpdt.TituloEspecialidade;
+            }
+
+            _healthClinicContext.Especialidade.Update(especialidadeBuscada!);
+
+            _healthClinicContext.SaveChanges();
+        }
+
+        public Especialidade BuscarPorId(Guid id)
+        {
+            return _healthClinicContext.Especialidade.FirstOrDefault(u => u.IdEspecialidade == id)!;
+        }
+
+        public void Cadastrar(Especialidade especialidadeCrt)
+        {
+            _healthClinicContext.Especialidade.Add(especialidadeCrt);
+
+            _healthClinicContext.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            try
+            {
+                Especialidade especialidadeDlt = _healthClinicContext.Especialidade.FirstOrDefault(u => u.IdEspecialidade == id)!;
+
+                _healthClinicContext.Especialidade.Remove(especialidadeDlt);
+
+                _healthClinicContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Especialidade> Listar()
+        {
+            return _healthClinicContext.Especialidade.ToList();
+        }
     }
 }
