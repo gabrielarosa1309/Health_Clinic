6bb4fa16fb5584fd32810f9476eba8736ef9f31e
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-10-02 11:00:35 -0300

    LoginController iniciado

---
+++ webApi.health_clinic/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using webApi.health_clinic.Domains;
+using webApi.health_clinic.Interfaces;
+using webApi.health_clinic.Repositories;
+using webApi.health_clinic.ViewModels;
+
+namespace webApi.health_clinic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class LoginController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public LoginController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPost]
+        public IActionResult Post(LoginViewModel usuario)
+        {
+            try
+            {
+                Usuario usuarioBuscado = _usuarioRepository.BuscarPorCPFeSenha(usuario.CPF!, usuario.Senha!);
+
+                if (usuarioBuscado == null)
+                {
+                    return StatusCode(401, "CPF ou senha inválidos!");
+                }
+
+                //lógica do token:
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Email, usuarioBuscado.Email!),
+                    new Claim(JwtRegisteredClaimNames.Name, usuarioBuscado.NomeUsuario!),
+                    new Claim(JwtRegisteredClaimNames.Jti,usuarioBuscado.IdUsuario.ToString()),
+                    new Claim(ClaimTypes.Role,usuarioBuscado.TiposUsuario!.Titulo!)
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("projeto-health_clinic-webapi-chave-autenticacao"));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                        issuer: "webApi.health_clinic.manha",
+                        audience: "webApi.health_clinic.manha",
+                        claims: claims,
+                        expires: DateTime.Now.AddMinutes(5),
+                        signingCredentials: creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            catch (Exception e) 
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
