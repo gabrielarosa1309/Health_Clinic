3e28054dc1f8b97f29b3afb83aa7430c659bdc69
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-10-04 09:11:48 -0300

    Repository e Controller de Consulta concluídos

--- webApi.health_clinic/Controllers/ConsultaController.cs
+++ webApi.health_clinic/Controllers/ConsultaController.cs
@@ -1,6 +1,92 @@
-﻿namespace webApi.health_clinic.Controllers
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using webApi.health_clinic.Domains;
+using webApi.health_clinic.Interfaces;
+using webApi.health_clinic.Repositories;
+
+namespace webApi.health_clinic.Controllers
 {
-    public class ConsultaController
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ConsultaController : ControllerBase
     {
+        private ConsultaRepository _ConsultaRepository;
+
+        public ConsultaController()
+        {
+            _ConsultaRepository = new ConsultaRepository();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Guid id, Consulta consultaUpdt)
+        {
+            try
+            {
+                _ConsultaRepository.Atualizar(id, consultaUpdt);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(Guid id)
+        {
+            try
+            {
+                return Ok(_ConsultaRepository.BuscarPorId(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Consulta consultaCrt)
+        {
+            try
+            {
+                _ConsultaRepository.Cadastrar(consultaCrt);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _ConsultaRepository.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_ConsultaRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
--- webApi.health_clinic/Repositories/ConsultaRepository.cs
+++ webApi.health_clinic/Repositories/ConsultaRepository.cs
@@ -1,6 +1,65 @@
-﻿namespace webApi.health_clinic.Repositories
+﻿using webApi.health_clinic.Contexts;
+using webApi.health_clinic.Domains;
+using webApi.health_clinic.Interfaces;
+using webApi.health_clinic.Utils;
+
+namespace webApi.health_clinic.Repositories
 {
-    public class ConsultaRepository
+    public class ConsultaRepository : IConsultaRepository
     {
+        private readonly HealthClinicContext _healthClinicContext;
+
+        public ConsultaRepository()
+        {
+            _healthClinicContext = new HealthClinicContext();
+        }
+
+        public void Atualizar(Guid id, Consulta consultaUpdt)
+        {
+            Consulta consultaBuscada = _healthClinicContext.Consulta.Find(id)!;
+
+            if (consultaBuscada != null)
+            {
+                consultaBuscada.DatetimeConsulta = consultaUpdt.DatetimeConsulta;
+                consultaBuscada.DescricaoConsulta = consultaUpdt.DescricaoConsulta;
+            }
+
+            _healthClinicContext.Consulta.Update(consultaBuscada!);
+
+            _healthClinicContext.SaveChanges();
+        }
+
+        public Consulta BuscarPorId(Guid id)
+        {
+            return _healthClinicContext.Consulta.FirstOrDefault(u => u.IdConsulta == id)!;
+        }
+
+        public void Cadastrar(Consulta consultaCrt)
+        {
+            _healthClinicContext.Consulta.Add(consultaCrt);
+
+            _healthClinicContext.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            try
+            {
+                Consulta consultaDlt = _healthClinicContext.Consulta.FirstOrDefault(u => u.IdConsulta == id)!;
+
+                _healthClinicContext.Consulta.Remove(consultaDlt);
+
+                _healthClinicContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Consulta> Listar()
+        {
+            return _healthClinicContext.Consulta.ToList();
+        }
     }
 }
