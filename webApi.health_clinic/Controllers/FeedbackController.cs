d859b602ff0cc8b201f1258142e969040330742c
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-10-04 08:59:51 -0300

    Repository e Controller de Feedback concluídos

--- webApi.health_clinic/Controllers/FeedbackController.cs
+++ webApi.health_clinic/Controllers/FeedbackController.cs
@@ -1,6 +1,93 @@
-﻿namespace webApi.health_clinic.Controllers
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using webApi.health_clinic.Domains;
+using webApi.health_clinic.Interfaces;
+using webApi.health_clinic.Repositories;
+
+namespace webApi.health_clinic.Controllers
 {
-    public class FeedbackController
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class FeedbackController : ControllerBase
     {
+        private FeedbackRepository _FeedbackRepository;
+
+        public FeedbackController()
+        {
+            _FeedbackRepository = new FeedbackRepository();
+        }
+
+        [HttpPut]
+        public IActionResult Put(Guid id, Feedback feedbackUpdt)
+        {
+            try
+            {
+                _FeedbackRepository.Atualizar(id, feedbackUpdt);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(Guid id)
+        {
+            try
+            {
+                return Ok(_FeedbackRepository.BuscarPorId(id));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(Feedback feedbackCrt)
+        {
+            try
+            {
+                _FeedbackRepository.Cadastrar(feedbackCrt);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                _FeedbackRepository.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_FeedbackRepository.Listar());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
 }
+ 
--- webApi.health_clinic/Repositories/FeedbackRepository.cs
+++ webApi.health_clinic/Repositories/FeedbackRepository.cs
@@ -1,6 +1,64 @@
-﻿namespace webApi.health_clinic.Repositories
+﻿using webApi.health_clinic.Contexts;
+using webApi.health_clinic.Domains;
+using webApi.health_clinic.Interfaces;
+using webApi.health_clinic.Utils;
+
+namespace webApi.health_clinic.Repositories
 {
-    public class FeedbackRepository
+    public class FeedbackRepository : IFeedbackRepository
     {
+        private readonly HealthClinicContext _healthClinicContext;
+
+        public FeedbackRepository()
+        {
+            _healthClinicContext = new HealthClinicContext();
+        }
+
+        public void Atualizar(Guid id, Feedback feedbackUpdt)
+        {
+            Feedback feedbackBuscado = _healthClinicContext.Feedback.Find(id)!;
+
+            if (feedbackBuscado != null)
+            {
+                feedbackBuscado.FeedbackConsulta = feedbackUpdt.FeedbackConsulta;
+            }
+
+            _healthClinicContext.Feedback.Update(feedbackBuscado!);
+
+            _healthClinicContext.SaveChanges();
+        }
+
+        public Feedback BuscarPorId(Guid id)
+        {
+            return _healthClinicContext.Feedback.FirstOrDefault(u => u.IdFeedback == id)!;
+        }
+
+        public void Cadastrar(Feedback feedbackCrt)
+        {
+            _healthClinicContext.Feedback.Add(feedbackCrt);
+
+            _healthClinicContext.SaveChanges();
+        }
+
+        public void Delete(Guid id)
+        {
+            try
+            {
+                Feedback feedbackDlt = _healthClinicContext.Feedback.FirstOrDefault(u => u.IdFeedback == id)!;
+
+                _healthClinicContext.Feedback.Remove(feedbackDlt);
+
+                _healthClinicContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Feedback> Listar()
+        {
+            return _healthClinicContext.Feedback.ToList();
+        }
     }
 }
